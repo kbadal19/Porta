b945d03f6c59d8ff2d4c8275c88ddd8f3309c5b7
kbadal19 <k.badal19@gmail.com>
2024-06-10 15:43:55 +0530

    meow

--- src/Components/TopNavbar.js
+++ src/Components/TopNavbar.js
@@ -126,7 +126,7 @@ export default function Navbar() {
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                     >
-                      Sign out
+                      {/* Sign out */}
                     </a>
                   </div>
                 </div>
